8169c35bf1a60f8414294eb5ae9334ac27a36c1b
Dante <beyondmind9@gmail.com>
2024-01-24 02:26:52 -0300

    ajustes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tsConfigPaths()],
   test: {
-    environmentMatchGlobs: [['src/http/controllers/**', 'prisma']]
+    environmentMatchGlobs: [
+      ['src/http/controllers/**', 'prisma']
+    ]
   }
 })
